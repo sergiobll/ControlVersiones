7463a71e7965ef59f5a6702dd0851064a7d230a8
SergioBarrigonLajo <sergio.barlaj@educa.jcyl.es>
2024-04-25 18:39:05 +0200

    Tercer commit

---
+++ src/main/java/com/mycompany/git/Git.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.git;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergio
+ */
+public class Git {
+public static void main(String[] args) {    
+    Scanner en = new Scanner(System.in);
+
+        int pass = 0;
+
+        while (pass != 1234){
+
+            System.out.println("Introduce la contraseña");
+            pass = en.nextInt();
+
+            if (pass == 1234) {
+                System.out.println("Contraseña correcta");
+
+            }else {
+                System.out.println("Contraseña incorrecta");
+            }
+        }
+
+}
+}
